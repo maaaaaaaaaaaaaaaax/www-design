acb47d375db929b31654ac46e8791b8410680b1a
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2025-10-29 11:15:54 +0100

    tech: remove comment, add missing bracket

--- src/modules/render/fetch.js
+++ src/modules/render/fetch.js
@@ -1,5 +1,4 @@
 export async function fetchSortedFiles(content) {
-  // Content now contains metadata, so we just need to process and sort it
   const validFiles = content.filter((item) => item && item.fileType && item.fileType !== "null");
 
   const sortedFiles = validFiles
